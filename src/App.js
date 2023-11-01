ee15793920b7ccda43c98aa6e06c36e4f3e50777
AbimanyuBP <70093819+AbimanyuBP@users.noreply.github.com>
2023-11-01 11:50:30 +0700

    Added basic routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 
+const router = createBrowserRouter(
+  // TODO: change it to object based routing in the future K...
+  createRoutesFromElements(
+    <Route path="/">
+        <Route index element={<Home/>}/>
+        <Route path="login" element={<Login/>}/>
+        <Route path="users">
+          <Route index element={<List/>}/>
+          <Route path=":userId" element={<Single/>}/>
+          <Route path="new" element={<New/>}/>
+        </Route>
+        {/* This admin template manages products */}
+        <Route path="products">
+          <Route index element={<List/>}/>
+          <Route path=":productId" element={<Single/>}/>
+          <Route path="new" element={<New/>}/>
+        </Route>
+    </Route>
+  )
+);
 
 function App() {
   return (
     <div className="App">
-      AAAAAAAAAAAAAAAAAAAAAAAAAA
+      <RouterProvider router={router} />
     </div>
   );
 }
