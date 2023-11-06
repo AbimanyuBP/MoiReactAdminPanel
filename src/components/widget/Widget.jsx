5d1261650cd4e1bf813484b8c87a17a3d3a3fb0e
Abimanyu B.P <abimanyubevanp@gmail.com>
2023-11-06 12:42:57 +0700

    Adding logic to Widget

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,21 +1,68 @@
 import "./widget.scss"
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
 
-const widget = () => {
+const widget = ({type}) => {
+
+  let data;
+
+  // Temporary
+  const amount = 100;
+  const diff = 20;
+
+  switch(type){
+    case "user":
+      data = {
+        title: "USERS",
+        isMoney: false,
+        link: "See all users",
+        icon: <PermIdentityIcon className="icon"/>,
+      }
+      break;
+    case "order":
+      data = {
+        title: "ORDERS",
+        isMoney: false,
+        link: "View all orders",
+        icon: <ShoppingCartOutlinedIcon className="icon"/>,
+      }
+      break;
+    case "earning":
+      data = {
+        title: "EARNINGS",
+        isMoney: true,
+        link: "View net earnings",
+        icon: <MonetizationOnOutlinedIcon className="icon"/>,
+      }
+      break;
+    case "balance":
+      data = {
+        title: "BALANCE",
+        isMoney: true,
+        link: "See details",
+        icon: <AccountBalanceWalletOutlinedIcon className="icon"/>,
+      }
+      break;
+    default:
+      break;
+  }
+
   return (
     <div className="widget">
         <div className="left">
-            <span className="title">USERS</span>
-            <span className="counter">23123</span>
-            <span className="link">See all users</span>
+            <span className="title">{data.title}</span>
+            <span className="counter">{data.isMoney && "$"} {amount}</span>
+            <span className="link">{data.link}</span>
         </div>
         <div className="right">
-            <div className="percentage">
+            <div className="percentage positive">
                 <KeyboardArrowUpIcon/>
                 20%
             </div>
-            <PermIdentityIcon/>
+            <PermIdentityIcon className="icon"/>
         </div>
     </div>
   )
