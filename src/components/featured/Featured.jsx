01334776dde87f08d428a57ca61bbdf2e5b80051
AbimanyuBP <70093819+AbimanyuBP@users.noreply.github.com>
2023-11-07 13:56:12 +0700

    Added Featured component design

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -22,10 +22,26 @@ const Featured = () => {
                 Previous transactions processing. Last payments may not be included
             </p>
             <div className="summary">
-                <div className="itemTitle">Target</div>
-                <div className="itemResult">
-                    <KeyboardArrowDownIcon fontSize='small'/>
-                    <div className="resultAmount">$12.4k</div>
+                <div className="item">
+                    <div className="itemTitle">Target</div>
+                    <div className="itemResult positive">
+                        <KeyboardArrowUpOutlinedIcon fontSize='small'/>
+                        <div className="resultAmount">$12.4k</div>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="itemTitle">Last Week</div>
+                    <div className="itemResult positive">
+                        <KeyboardArrowUpOutlinedIcon fontSize='small'/>
+                        <div className="resultAmount">$12.4k</div>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="itemTitle">Last Month</div>
+                    <div className="itemResult negative">
+                        <KeyboardArrowDownIcon fontSize='small'/>
+                        <div className="resultAmount">$12.4k</div>
+                    </div>
                 </div>
             </div>
         </div>
