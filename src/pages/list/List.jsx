d5aa806ebc5c369814c9637dee6dfb6f11a74db0
AbimanyuBP <70093819+AbimanyuBP@users.noreply.github.com>
2023-11-08 16:35:50 +0700

    Added User datatable design

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import "./list.scss"
 import { Outlet } from 'react-router'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
+import Datatable from '../../components/datatable/Datatable'
 
 const List = () => {
   return (
     <div className='list'>
-      List
-
+      <Sidebar/>
+      <div className="listContainer">
+        <Navbar/>
+        <Datatable/>
+      </div>
     </div>
   )
 }
